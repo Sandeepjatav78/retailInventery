b75191893e1b2e6a6cb6e42b775cb7bc12264c22
Sandeep <sy268134@gmail.com>
2025-12-21 21:57:27 +0530

    api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}api`, // Your Backend URL
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`, // Your Backend URL
 });
 
 export default instance;
