ef6f976a7258791e84647ddd7482681a9fdba97a
Sandeep <sy268134@gmail.com>
2025-12-21 21:47:43 +0530

    backend link add

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your Backend URL
+  baseURL: import.meta.env.VITE_BACKEND_URL, 
 });
+
 
 export default instance;
